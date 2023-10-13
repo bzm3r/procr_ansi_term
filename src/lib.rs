066bd5746f6f58a520f0ba14a84d53aeedfe45f8
Brian Merchant <bhmerchant@gmail.com>
2023-10-13 11:03:44 -0700

    Merge remote-tracking branch 'nu-ansi-term/main' into const-update

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,47 @@
-//! This is a library for controlling colors and formatting, such as
-//! red bold text or blue underlined text, on ANSI terminals.
+//! This is a library for controlling colors and formatting, such as red bold
+//! text or blue underlined text, on ANSI terminals.
+//!
+//! ## Should I use this crate?
+//!
+//! Short answer: no.
+//! [`owo_colors`](https://docs.rs/owo-colors/latest/owo_colors/) does
+//! everything this crate does, but more rationally, and more effectively.
+//!
+//! ## History
+//!
+//! [`nu-ansi-term`](https://github.com/nushell/nu-ansi-term) is used by
+//! [`tracing_subscriber`](https://github.com/tokio-rs/tracing/tree/master/tracing-subscriber)
+//! (for what may be considered [historical
+//! reasons](https://github.com/tokio-rs/tracing/pull/2040)), and was a copy of
+//! `ansi_term` but with `Colour` changed to `Color` and various colors added.
+//! `procr-ansi-term`, born in a time when I didn't know better, adds
+//! functionality to `nu-ansi-term` which allows ANSI formatting of
+//! `format_args!`, and also allows various ANSI strings/format args to be
+//! nested in styling, with a primitive parent-child inheritance of said
+//! styling.
+//!
+//! It is ultimately best understood as the result of hyperfocus/procrastination
+//! (hence the name `procr_ansi_term`).
+//!
+//! ## What is this crate useful for?
+//!
+//! Styling data that will be eventually rendered by a terminal capable of
+//! interpreting ANSI style codes.
 //!
 //!
 //! ## Basic usage
 //!
-//! There are three main types in this crate that you need to be
-//! concerned with: [`AnsiString`], [`Style`], and [`Color`].
+//! There are a few basic types: [`Style`](crate::style::Style), [`Color`](crate::style::Color), [`AnsiGenericString`],
+//! and [`Content`].
 //!
-//! A `Style` holds stylistic information: foreground and background colors,
+//! A [`Style`] holds stylistic information: foreground and background colors,
 //! whether the text should be bold, or blinking, or other properties. The
 //! [`Color`] enum represents the available colors. And an [`AnsiString`] is a
 //! string paired with a [`Style`].
 //!
-//! [`Color`] is also available as an alias to `Color`.
-//!
-//! To format a string, call the `paint` method on a `Style` or a `Color`,
-//! passing in the string you want to format as the argument. For example,
+//! To format some [`Content`], call the `paint` method on a [`Style`] or a `Color`,
+//! passing in some input that can be converted into [`Content`] (`impl
+//! Into<Content<'a, S>>`) you want to format as the argument. For example,
 //! here’s how to get some red text:
 //!
 //! ```
@@ -24,11 +50,10 @@
 //! println!("This is in red: {}", Red.paint("a red string"));
 //! ```
 //!
-//! It’s important to note that the `paint` method does *not* actually return a
+//! The `paint` method does not return a
 //! string with the ANSI control characters surrounding it. Instead, it returns
-//! an [`AnsiString`] value that has a [`Display`] implementation that, when
-//! formatted, returns the characters. This allows strings to be printed with a
-//! minimum of [`String`] allocations being performed behind the scenes.
+//! an [`AnsiString`] value that has a [`Display`](std::fmt::Display) (note: not [`Debug`](std::fmt::Debug)! implementation that, when
+//! formatted, returns the characters.
 //!
 //! If you *do* want to get at the escape codes, then you can convert the
 //! [`AnsiString`] to a string as you would any other `Display` value:
@@ -56,9 +81,9 @@
 //!          Style::new().underline().paint("underline"));
 //! ```
 //!
-//! For brevity, these methods have also been implemented for `Color` values,
-//! so you can give your styles a foreground color without having to begin with
-//! an empty `Style` value:
+//! For brevity, these methods have also been implemented for `Color` values, so
+//! you can give your styles a foreground color without having to begin with an
+//! empty `Style` value:
 //!
 //! ```
 //! use procr_ansi_term::Color::{Blue, Yellow};
@@ -70,13 +95,13 @@
 //! println!("Yellow on blue: {}", Yellow.on_bg(Blue).paint("wow!"));
 //! ```
 //!
-//! The complete list of styles you can use are: [`bold`], [`dimmed`], [`italic`],
-//! [`underline`], [`blink`], [`reverse`], [`hidden`], [`strikethrough`], and [`on`] for
-//! background colors.
+//! The complete list of styles you can use are: [`bold`], [`dimmed`],
+//! [`italic`], [`underline`], [`blink`], [`reverse`], [`hidden`],
+//! [`strikethrough`], and [`on`] for background colors.
 //!
 //! In some cases, you may find it easier to change the foreground on an
-//! existing `Style` rather than starting from the appropriate `Color`.
-//! You can do this using the [`fg`] method:
+//! existing `Style` rather than starting from the appropriate `Color`. You can
+//! do this using the [`fg`] method:
 //!
 //! ```
 //! use procr_ansi_term::Style;
@@ -86,13 +111,14 @@
 //! println!("Also yellow on blue: {}", Cyan.on_bg(Blue).fg(Yellow).paint("zow!"));
 //! ```
 //!
-//! You can turn a `Color` into a `Style` with the [`normal`] method.
-//! This will produce the exact same `AnsiString` as if you just used the
-//! `paint` method on the `Color` directly, but it’s useful in certain cases:
-//! for example, you may have a method that returns `Styles`, and need to
-//! represent both the “red bold” and “red, but not bold” styles with values of
-//! the same type. The `Style` struct also has a [`Default`] implementation if you
-//! want to have a style with *nothing* set.
+//! You can turn a [`Color`] into a [`Style`] with the [`as_fg`](crate::color::as_fg) or
+//! [`as_bg`](crate::Color::as_bg) methods. This will
+//! produce the exact same `AnsiString` as if you just used the `paint` method
+//! on the `Color` directly, but it’s useful in certain cases: for example, you
+//! may have a method that returns `Styles`, and need to represent both the “red
+//! bold” and “red, but not bold” styles with values of the same type. The
+//! `Style` struct also has a [`Default`] implementation if you want to have a
+//! style with *nothing* set.
 //!
 //! ```
 //! use procr_ansi_term::Style;
@@ -118,8 +144,7 @@
 //!
 //! The first sixteen of these values are the same as the normal and bold
 //! standard color variants. There’s nothing stopping you from using these as
-//! `Fixed` colors instead, but there’s nothing to be gained by doing so
-//! either.
+//! `Fixed` colors instead, but there’s nothing to be gained by doing so either.
 //!
 //! You can also access full 24-bit color by using the `Color::Rgb` variant,
 //! which takes separate `u8` arguments for red, green, and blue:
@@ -132,19 +157,10 @@
 //!
 //! ## Combining successive colored strings
 //!
-//! The benefit of writing ANSI escape codes to the terminal is that they
-//! *stack*: you do not need to end every colored string with a reset code if
-//! the text that follows it is of a similar style. For example, if you want to
-//! have some blue text followed by some blue bold text, it’s possible to send
-//! the ANSI code for blue, followed by the ANSI code for bold, and finishing
-//! with a reset code without having to have an extra one between the two
-//! strings.
-//!
-//! This crate can optimise the ANSI codes that get printed in situations like
-//! this, making life easier for your terminal renderer. The [`AnsiStrings`]
-//! type takes a slice of several [`AnsiString`] values, and will iterate over
-//! each of them, printing only the codes for the styles that need to be updated
-//! as part of its formatting routine.
+//! This crate can (somewhat) optimise the ANSI codes that get printed in
+//! situations where it knows exactly which [`AnsiGenericString`]s are to be
+//! printed in sequence. Such a sequence is held by the type:
+//! [`AnsiGenericStrings`].
 //!
 //! The following code snippet uses this to enclose a binary number displayed in
 //! red bold text inside some red, but not bold, brackets:
@@ -154,32 +170,23 @@
 //! use procr_ansi_term::{AnsiString, AnsiStrings};
 //!
 //! let some_value = format!("{:b}", 42);
-//! let strings: &[AnsiString<'static>] = &[
+//! let strings: [AnsiString<'static>] = [
 //!     Red.paint("["),
 //!     Red.bold().paint(some_value),
 //!     Red.paint("]"),
 //! ];
 //!
-//! println!("Value: {}", AnsiStrings(strings.iter().cloned()));
+//! println!("Value: {}", AnsiStrings(strings));
 //! ```
-//!
-//! There are several things to note here. Firstly, the [`paint`] method can take
-//! *either* an owned [`String`] or a borrowed [`&str`]. Internally, an [`AnsiString`]
-//! holds a copy-on-write ([`Cow`]) string value to deal with both owned and
-//! borrowed strings at the same time. This is used here to display a `String`,
-//! the result of the `format!` call, using the same mechanism as some
-//! statically-available `&str` slices. Secondly, that the [`AnsiStrings`] value
-//! works in the same way as its singular counterpart, with a [`Display`]
-//! implementation that only performs the formatting when required.
 //!
 //! ## Byte strings
 //!
 //! This library also supports formatting `\[u8]` byte strings; this supports
 //! applications working with text in an unknown encoding.  [`Style`] and
-//! [`Color`] support painting `\[u8]` values, resulting in an [`AnsiByteString`].
-//! This type does not implement [`Display`], as it may not contain UTF-8, but
-//! it does provide a method [`write_to`] to write the result to any value that
-//! implements [`Write`]:
+//! [`Color`] support painting `\[u8]` values, resulting in an
+//! [`AnsiByteString`]. This type does not implement [`Display`], as it may not
+//! contain UTF-8, but it does provide a method [`write_to`] to write the result
+//! to any value that implements [`Write`]:
 //!
 //! ```
 //! use procr_ansi_term::Color::Green;
@@ -199,35 +206,6 @@
 //!     Green.bold().paint("user data 2\n".as_bytes()),
 //! ]).write_to(&mut std::io::stdout()).unwrap();
 //! ```
-//!
-//! [`Cow`]: https://doc.rust-lang.org/std/borrow/enum.Cow.html
-//! [`Display`]: https://doc.rust-lang.org/std/fmt/trait.Display.html
-//! [`Default`]: https://doc.rust-lang.org/std/default/trait.Default.html
-//! [`String`]: https://doc.rust-lang.org/std/string/struct.String.html
-//! [`&str`]: https://doc.rust-lang.org/std/primitive.str.html
-//! [`Write`]: https://doc.rust-lang.org/std/io/trait.Write.html
-//! [`Style`]: struct.Style.html
-//! [`Style::new()`]: struct.Style.html#method.new
-//! [`Color`]: enum.Color.html
-//! [`Color`]: enum.Color.html
-//! [`AnsiString`]: type.AnsiString.html
-//! [`AnsiStrings`]: type.AnsiStrings.html
-//! [`AnsiByteString`]: type.AnsiByteString.html
-//! [`AnsiByteStrings`]: type.AnsiByteStrings.html
-//! [`write_to`]: type.AnsiByteString.html#method.write_to
-//! [`paint`]: type.AnsiByteString.html#method.write_to
-//! [`normal`]: enum.Color.html#method.normal
-//!
-//! [`bold`]: struct.Style.html#method.bold
-//! [`dimmed`]: struct.Style.html#method.dimmed
-//! [`italic`]: struct.Style.html#method.italic
-//! [`underline`]: struct.Style.html#method.underline
-//! [`blink`]: struct.Style.html#method.blink
-//! [`reverse`]: struct.Style.html#method.reverse
-//! [`hidden`]: struct.Style.html#method.hidden
-//! [`strikethrough`]: struct.Style.html#method.strikethrough
-//! [`fg`]: struct.Style.html#method.fg
-//! [`on`]: struct.Style.html#method.on
 
 #![crate_name = "procr_ansi_term"]
 #![crate_type = "rlib"]
